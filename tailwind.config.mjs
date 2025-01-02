6c66329140178ab798f9b41604b6a5d77487b899
DanieII <danielbogdanov1748@gmail.com>
2025-01-02 14:13:28 +0200

    Change css color variable name

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: "selector",
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    theme: {
-        extend: {
-            colors: {
-                "bg-color": "var(--bg-color)",
-                "secondary-bg-color": "var(--secondary-bg-color)",
-                "primary-color": "var(--primary-color)",
-                "secondary-color": "var(--secondary-color)",
-                "other-color": "var(--other-color)",
-            },
-        },
+  darkMode: "selector",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "bg-color": "var(--bg-color)",
+        "secondary-bg-color": "var(--secondary-bg-color)",
+        "primary-color": "var(--primary-color)",
+        "secondary-color": "var(--secondary-color)",
+        "accent-color": "var(--accent-color)",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
